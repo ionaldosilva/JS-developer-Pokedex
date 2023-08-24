e0326be7a17c2435be6c8a600f7224f652dfee9a
Ionaldo Silva <ionaldo_araujo@hotmail.com>
2023-08-24 00:47:29 -0300

    criado arquivos e funções JS/refatorado codigo

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+ 
+function convertPokemonToHtml(pokemon){
+    return `
+    
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        </div>
+    </li>
+
+    `       
+}
+
+const pokemonOL = document.getElementById('pokemonlist')
+
+pokeApi.getPokemons().then((pokemonlist = []) => {
+        pokemonOL.innerHTML += pokemonlist.map(convertPokemonToHtml).join('') 
+    }) 
+    
+
+
